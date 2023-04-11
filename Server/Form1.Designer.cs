eabf5bedb397d67ba00aa56e44affb3c667fb47f
tasnim alyasino <tasnimyasino1@gmail.com>
2023-04-11 10:26:46 +0200

    first commit

--- Server/Form1.Designer.cs
+++ Server/Form1.Designer.cs
@@ -28,19 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtDataToClient = new TextBox();
+            btnSendtoClient = new Button();
+            txtDataFromClient = new TextBox();
+            label3 = new Label();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // txtDataToClient
+            // 
+            txtDataToClient.Location = new Point(370, 122);
+            txtDataToClient.Multiline = true;
+            txtDataToClient.Name = "txtDataToClient";
+            txtDataToClient.Size = new Size(300, 154);
+            txtDataToClient.TabIndex = 6;
+            // 
+            // btnSendtoClient
+            // 
+            btnSendtoClient.Location = new Point(370, 282);
+            btnSendtoClient.Name = "btnSendtoClient";
+            btnSendtoClient.Size = new Size(82, 26);
+            btnSendtoClient.TabIndex = 8;
+            btnSendtoClient.Text = "Send";
+            btnSendtoClient.UseVisualStyleBackColor = true;
+            btnSendtoClient.Click += btnSendtoClient_Click;
+            // 
+            // txtDataFromClient
+            // 
+            txtDataFromClient.Location = new Point(12, 122);
+            txtDataFromClient.Multiline = true;
+            txtDataFromClient.Name = "txtDataFromClient";
+            txtDataFromClient.Size = new Size(300, 154);
+            txtDataFromClient.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(370, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 17);
+            label3.TabIndex = 10;
+            label3.Text = "Data zum Client";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 17);
+            label1.TabIndex = 11;
+            label1.Text = "Data vom Client";
             // 
             // ServerForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 510);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(txtDataFromClient);
+            Controls.Add(btnSendtoClient);
+            Controls.Add(txtDataToClient);
             Name = "ServerForm";
             Text = "Server";
             Load += ServerForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtDataToClient;
+        private Button btnSendtoClient;
+        private TextBox txtDataFromClient;
+        private Label label3;
+        private Label label1;
     }
 }
--- Server/Form1.cs
+++ Server/Form1.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
@@ -10,7 +11,9 @@ namespace Server
         private Socket socket;
         private Socket client;
         private int PORT_NUM = 50000;
-        
+        private byte[] data;
+        private int bytesReceived;
+
         public ServerForm()
         {
             InitializeComponent();
@@ -25,9 +28,39 @@ namespace Server
             socket.Listen(10);
             client = socket.Accept();
 
-            
+            Thread receiveThread = new Thread(new ThreadStart(ReceiveData));
+            receiveThread.Start();
 
+        }
+        private void ReceiveData()
+        {
+            while (true)
+            {
+                try
+                {
+                    // Receive data from client
+                    data = new byte[1024];
+                    bytesReceived = client.Receive(data);
+                    string message = Encoding.ASCII.GetString(data, 0, bytesReceived);
 
+                    // Update UI with received message
+                    Invoke((MethodInvoker)delegate
+                    {
+                        txtDataFromClient.AppendText("Client: " + message + Environment.NewLine);
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+            }
+        }
+
+        private void btnSendtoClient_Click(object sender, EventArgs e)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(txtDataToClient.Text);
+            client.Send(data);
         }
     }
 }
